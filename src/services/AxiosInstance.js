e5619e0fe3e373a9cfaea711e65d1ea1cee99ab0
Okunade Olaitan <olaokunadeola@gmail.com>
2024-10-30 10:14:01 +0100

    latest

--- src/services/AxiosInstance.js
+++ src/services/AxiosInstance.js
@@ -3,11 +3,11 @@ import axios from 'axios';
 
 
 // *** API SETUP ***/
-const token= JSON.parse(localStorage.getItem('cinema-auth-session'))?.state?.userData?.accessToken;
 
 const API = axios.create({ baseURL: `http://localhost:3000/api/` });
 
 API.interceptors.request.use((req) => {
+  const token= JSON.parse(localStorage.getItem('cinema-auth-session'))?.state?.userData?.accessToken;
   req.headers['Authorization'] = `Bearer ${token}`;
   req.headers['Content-type'] = 'application/json';
   req.headers["Accept"] = 'application/json';
